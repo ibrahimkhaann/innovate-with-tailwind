384d9c3d144260a14cd82058459f5134d766f031
ibrahimkkhaann <you@example.com>
2025-01-12 18:11:18 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,52 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        bgColor: 'rgba(22, 12, 109, 0.6)',
+        hoverColor: '#1BBF00',
+        bannerOverly: ' rgba(106, 98, 197, 0.8);',
+        paprikaColor: '#E6E5F3',
+        bodycolor: '#C4C4C4',
+        bordercolor: '#4F585F',
+        projectcolor: '#1BBF00',
+        clientcolor: '#141135',
+        clientgol: '#CBC8F0',
+        contactcolor: '#726E9E',
+        why: '#141135',
+        teamcolor: '#726E9E',
+        whatbody: '#F6F5FF',
+        dev: '#6B62C5',
+        texthover: '#1BBF00',
+        plan: 'rgba(249, 249, 251, 0.694);',
+        planB: '#F8F8FF',
+        planc: '#FF7628',
+        plane: '#48409C',
+        planf: '#FEFEFE',
+        pland: 'rgba(25, 191, 0, 0.3);',
+
+
+
+
+      },
+      fontFamily: {
+        open: ['Open Sans'],
+        paprika: ['Paprika'],
+        Quicksand: ['Quicksand'],
+      },
+      maxWidth: {
+        container: '1170px',
+      },
+      backgroundImage: {
+        'bannerimg': "url('./images/banner.jpg')",
+        'backimg': "url('./images/bg10.png')",
+        'nicherimg': "url('./images/bg11.png')",
+        'lastimg': "url('./images/bg (3).png')",
+      },
+      boxShadow: {
+        'body': '3px 3px 5px 0px rgba(0,0,0,0.75);',
+        'body2': '5px 5px 20px 0px rgba(0,0,0,0.75);',
+      },
+    },
+  },
+}
